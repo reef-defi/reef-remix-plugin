b1713c336ada232a197772ca3fb45ad5cb808f51
Žiga Franko <franko.ziga@gmail.com>
2021-05-03 12:15:26 +0200

    webpack configuration

---
+++ webpack/webpack.config.js
@@ -0,0 +1,8 @@
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common.js");
+
+module.exports = ({env}) => {
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, envConfig);
+  return config
+}
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    hot: true,
+    open: true
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Developement')
+    })
+  ]
+}
