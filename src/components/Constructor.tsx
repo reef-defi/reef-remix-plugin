92490f71b15eeabf123dafa6a14177f7111331e2
Žiga Franko <franko.ziga@gmail.com>
2021-05-11 07:18:19 +0200

    Account selector

--- src/components/Constructor.tsx
+++ src/components/Constructor.tsx
@@ -1,29 +1,52 @@
 import React, { useEffect, useState } from "react"
 
-// import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
+import { isWeb3Injected, web3Accounts, web3Enable } from "@polkadot/extension-dapp";
+import { RemixSigner } from "../state/signers";
 
 interface ConstructorProps {
-
+  signers: RemixSigner [];
 }
 
-const Constructor = ({} : ConstructorProps) => {
-  const [text, setText] = useState("Loading");
+const Constructor = ({signers} : ConstructorProps) => {
+  const [account, setAccount] = useState(signers.length > 0 
+    ? signers[0].address 
+    : ""
+  );
 
-  useEffect(() => {
-    // web3Enable("reef-plugin")
-    //   .then(() => {
-    //     setText("Loaded successfully")
-    //   })
-    //   .catch((err) => {
-    //     setText("Error: " + err.message);
-    //   });
-  }, [])
+  // useEffect(() => {
+  //   console.log(isWeb3Injected);
+  //   web3Enable("reef-remix-plugin")
+  //     .then(() => web3Accounts())
+  //     .then((accounts) => {
+  //       console.log(accounts)
+  //       setText("Loaded successfully")
+  //     })
+  //     .catch((err) => {
+  //       setText("Error: " + err.message);
+  //     });
+  // }, [])
+
+  const signerOptions = signers
+    .map(({address}) => (
+      <option value={address}>{address}</option>
+    ));
 
   return (
     <div className="m-3">
-      hello
-      <br/>
-      {text}
+      <div>
+        <label>
+          Accounts:
+        </label>
+
+        <select
+          id="accountSelector"
+          className="form-select"
+          defaultValue={account}
+          onChange={(event) => setAccount(event.target.value)}
+        >
+          { signerOptions }
+        </select>
+      </div>
     </div>
   );
 }
