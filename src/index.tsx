5355ed62140810fb50d00507792782f3a135dcf1
Žiga Franko <franko.ziga@gmail.com>
2021-05-11 09:09:51 +0200

    injecting compiled contract

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Dispatch } from "react";
 import ReactDOM from "react-dom";
-import { PluginClient } from "@remixproject/plugin";
+import { Client, PluginClient } from "@remixproject/plugin";
 import { createClient } from "@remixproject/plugin-iframe";
 
 import { web3Enable, web3Accounts, web3EnablePromise, isWeb3Injected } from "@polkadot/extension-dapp";
-
+import { IRemixApi } from '@remixproject/plugin-api';
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "./store";
+import { Provider } from "react-redux";
+import { StateType } from "./store/reducers";
+import { contractLoad } from "./store/actions/contracts";
 
 declare global {
   interface Window {
@@ -15,6 +19,10 @@ declare global {
   }
 };
 
+const store = configureStore();
+
+type IClient = Client<any, Readonly<IRemixApi>>;
+type IDispatch = typeof store.dispatch;
 
 const client = createClient(new PluginClient());
 
@@ -22,11 +30,24 @@ const client = createClient(new PluginClient());
 client.onload(async () => {
   ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
     document.getElementById('root')
-    );
+  );
     
-    reportWebVitals();
+  reportWebVitals();
+
+  await initPlugin(client, store.dispatch);
 });
 
+const initPlugin = async (client: IClient, dispatch: IDispatch) => {
+  const result = await client.solidity.getCompilationResult()
+  dispatch(contractLoad(result.data));
+
+  client.solidity.on('compilationFinished', async () => {
+    const result = await client.solidity.getCompilationResult()
+    dispatch(contractLoad(result.data));
+  })
+}
