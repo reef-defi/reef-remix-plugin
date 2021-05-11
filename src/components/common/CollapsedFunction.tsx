4f3d42b2d1b08924643628d19f733ed43ed3b2e7
Žiga Franko <franko.ziga@gmail.com>
2021-05-11 22:28:39 +0200

    Common CollapsedFunction and InlineFunction

---
+++ src/components/common/CollapsedFunction.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import { ABIParameter } from "@remixproject/plugin-api/lib/compiler/type";
+
+interface CollapsedFunctionProps {
+  parameters: ABIParameter[];
+  errorMessage: string;
+  onClose: () => void;
+  submit: (value: string[]) => Promise<void>;
+}
+
+const CollapsedFunction = ({parameters, errorMessage, onClose, submit} : CollapsedFunctionProps) => {
+  const [values, setValues] = useState<string[]>(Array(parameters.length).fill(""));
+
+  const onChange = (value: string, index: number) => setValues([
+    ...values.slice(0, index),
+    value,
+    ...values.slice(index+1, values.length)
+  ]);
+
+  const attributesView = values
+    .map((value, index) => (
+      <div className="form-group row" key={index}>
+        <label className="col-3 lead m-auto">{parameters[index].name}</label>
+        <div className="col-9">
+          <input 
+            value={value}
+            placeholder={parameters[index].type}
+            className="form-control"
+            onChange={(event) => onChange(event.target.value, index)}
+          /> 
+        </div>
+      </div>
+    ));
+
+  return (
+    <div>
+      <div className="d-flex align-items-center justify-content-between">
+        <span className="text-light">DEPLOY</span>
+        <svg onClick={onClose} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-up text-light border rounded-circle ml-1" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+        </svg>
+      </div>
+
+      <div className="mt-1">
+        { attributesView }
+      </div>
+      
+      <div className="d-flex justify-content-end mt-1">
+        <a className="btn btn-outline-light btn-text" onClick={() => submit(values)}>
+          transact
+        </a>
+      </div>
+      { errorMessage && 
+        <div className="text-danger mt-2">{errorMessage}</div>
+      }
+    </div>
+  )
+}
+
+export default CollapsedFunction;
---
+++ src/components/common/InlineFunction.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react"
+import { useSelector } from "react-redux";
+import { StateType } from "../../store/reducers";
+import { ABIParameter } from "@remixproject/plugin-api/lib/compiler/type";
+
+interface InlineFunctionProps {
+  name: string;
+  errorMessage: string;
+  parameters: ABIParameter[];
+  onOpen: () => void;
+  submit: (value: string) => void;
+}
+
+const InlineFunction = ({name, parameters, errorMessage, submit, onOpen} : InlineFunctionProps) => {
+  const [value, setValue] = useState("");
+
+  const placeholder = parameters
+    .reduce((acc, inp) => (`${acc}${inp.type} ${inp.name}, `), "");
+
+  return (
+    <div>
+      <div className="d-flex flex-row align-items-center">
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <a className="btn btn-outline-light btn-text" onClick={() => submit(value)}>
+              { name }
+            </a>
+          </div>
+          <input 
+            value={value}
+            placeholder={placeholder.slice(0, placeholder.length-2)}
+            className="form-control"
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <svg onClick={onOpen} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-down text-light border rounded-circle ml-1" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+        </svg>
+      </div>
+      { errorMessage && 
+        <div className="text-danger mt-2">
+          {errorMessage}
+        </div>
+      }
+    </div>
+  );
+}
+
+export default InlineFunction;
