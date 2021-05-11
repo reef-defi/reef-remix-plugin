f9d9e6a0748910be77ce5bb172ff2fab747f988c
Žiga Franko <franko.ziga@gmail.com>
2021-05-11 07:18:43 +0200

    App stores RemixSigner

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import { WsProvider } from '@polkadot/api';
-import { Provider } from '@reef-defi/evm-provider';
+import { WsProvider, Keyring } from '@polkadot/api';
+import { Provider, Signer, TestAccountSigningKey } from '@reef-defi/evm-provider';
 import React, { useState } from 'react';
-import Loading from './components/Loading';
+import Constructor from './components/Constructor';
+import Loading from './components/common/Loading';
 import Network from './components/NetworkConfig';
+import { ReefSigner } from '@reef-defi/hardhat-reef/dist/src/proxies/signers/ReefSigner';
+import { KeyringPair } from "@polkadot/keyring/types";
+import { RemixSigner } from './state/signers';
+
+const createSeedKeyringPair = (seed: string): KeyringPair => {
+  const keyring = new Keyring({ type: "sr25519" });
+  return keyring.addFromUri(seed);
+};
+
+const extractAddress = async (wallet: ReefSigner): Promise<RemixSigner> => ({
+  address: await wallet.getAddress(),
+  signer: wallet
+});
+
+const connectWallets = (provider: Provider, mnemonics: string[]): ReefSigner[] => {
+  const signingKeys = new TestAccountSigningKey(provider.api.registry);
+  const pairs = mnemonics
+    .map((mnemonic) => createSeedKeyringPair(mnemonic));
+
+  signingKeys.addKeyringPair(pairs);
+
+  const signers = pairs
+    .map((pair) => new Signer(provider, pair.address, signingKeys));
+    
+  return signers;
+} 
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [provider, setProvider] = useState<Provider|undefined>(undefined);
   const [errorMessage, setErrorMessage] = useState("");
+  const [signers, setSigners] = useState<RemixSigner[]>([]);
 
-  const submit = async (url: string) => {
+  const submit = async (url: string, mnemonics: string[]) => {
     const newProvider = new Provider({
       provider: new WsProvider(url),
     });
@@ -18,6 +46,11 @@ const App = () => {
     try {
       await (await newProvider.resolveApi).isReady
       setProvider(newProvider);
+      const wallets = await Promise.all(
+        connectWallets(newProvider, mnemonics)
+          .map(extractAddress)
+      );
+      setSigners(wallets);
     } catch (e) {
       setErrorMessage(e.message);
       setProvider(undefined);
@@ -30,7 +63,7 @@ const App = () => {
     <div className="app">
       {isLoading && <Loading />}
       {(!isLoading && !provider) && <Network errorMessage={errorMessage} submit={submit} />}
-      {/* {(!isLoading && provider) && <Constructor />} */}
+      {(!isLoading && provider) && <Constructor signers={signers} />}
     </div>
   );
 }
