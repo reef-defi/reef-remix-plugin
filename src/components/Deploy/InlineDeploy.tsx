0cdcf052eed9af2e41ab019cd387b68290212c12
Žiga Franko <franko.ziga@gmail.com>
2021-05-11 22:29:28 +0200

    CollapsedDeploy and InlineDeploy refactored with new CollapsedFunction and InlineFunction

--- src/components/Deploy/InlineDeploy.tsx
+++ src/components/Deploy/InlineDeploy.tsx
@@ -4,6 +4,7 @@ import { getSigner, submitDeploy } from "../../api/contract";
 import { StateType } from "../../store/reducers";
 import { getConstructor, prepareContractParams } from "../../utils";
 import { CompiledContract, ABIParameter } from "@remixproject/plugin-api/lib/compiler/type";
+import InlineFunction from "../common/InlineFunction";
 
 interface InlineDeployProps {
   signerAddress: string;
@@ -14,7 +15,6 @@ interface InlineDeployProps {
 
 const InlineDeploy = ({ onOpen, contract, signerAddress } : InlineDeployProps) => {
   const dispatch = useDispatch();
-  const [value, setValue] = useState("");
   const {signers} = useSelector((state: StateType) => state.signers);
   const { errorMessage } = useSelector((state: StateType) => state.contracts);
 
@@ -22,41 +22,18 @@ const InlineDeploy = ({ onOpen, contract, signerAddress } : InlineDeployProps) =
   const signer = getSigner(signers, signerAddress);
   const parameters = abi.inputs! as ABIParameter[];
 
-  const submit = async () => {
+  const submit = async (value: string) => {
     const params = prepareContractParams(value, parameters);
     await submitDeploy(params, contract, signer.signer, dispatch);
   };
 
-  const placeholder = parameters
-    .reduce((acc, inp) => (`${acc}${inp.type} ${inp.name}, `), "");
-
-  return (
-    <div>
-      <div className="d-flex flex-row align-items-center">
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <a className="btn btn-outline-light btn-text" onClick={submit}>
-              Deploy
-            </a>
-          </div>
-          <input 
-            value={value}
-            placeholder={placeholder.slice(0, placeholder.length-2)}
-            className="form-control"
-            onChange={(event) => setValue(event.target.value)}
-          />
-        </div>
-        <svg onClick={onOpen} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-down text-light border rounded-circle ml-1" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-        </svg>
-      </div>
-      { errorMessage && 
-        <div className="text-danger mt-2">
-          {errorMessage}
-        </div>
-      }
-    </div>
-  );
+  return <InlineFunction
+    name="Deploy"
+    parameters={parameters}
+    errorMessage={errorMessage}
+    onOpen={onOpen}
+    submit={submit}
+  />
 }
 
 export default InlineDeploy;
