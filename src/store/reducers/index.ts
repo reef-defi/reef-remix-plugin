5aa157554f68de293893a6a35cc5b58008d97963
Žiga Franko <franko.ziga@gmail.com>
2021-05-11 16:15:15 +0200

    Signer reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,16 +1,19 @@
 import { combineReducers} from 'redux';
 import { ContractReducer, contractReducer } from './contracts';
+import { SignersReducer, signersReducer } from './signers';
 import { TransactionReducer, transactionReducer } from './transaction';
 
 
 const rootReducer = combineReducers({
   contracts: contractReducer,
   transactions: transactionReducer,
+  signers: signersReducer,
 });
 
 export interface StateType {
   contracts: ContractReducer,
   transactions: TransactionReducer,
+  signers: SignersReducer,
 }
 
 export default rootReducer;
