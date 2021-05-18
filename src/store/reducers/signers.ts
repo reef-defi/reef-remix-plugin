0829899cd2307edb88e6fd088789e86ba9f3b9ac
Žiga Franko <franko.ziga@gmail.com>
2021-05-18 12:09:13 +0200

    signers reducer now points to selected signer

--- src/store/reducers/signers.ts
+++ src/store/reducers/signers.ts
@@ -1,13 +1,15 @@
 import { RemixSigner } from "../../state/signers";
 import { SignersActionType } from "../actions/signers";
-import { SIGNERS_BALANCE, SIGNERS_LOAD } from "../actionType";
+import { SIGNERS_BALANCE, SIGNERS_LOAD, SIGNERS_SELECT } from "../actionType";
 
 
 export interface SignersReducer {
+  index: number;
   signers: RemixSigner[];
 }
 
 const initialState: SignersReducer = {
+  index: -1,
   signers: []
 };
 
@@ -15,14 +17,16 @@ export const signersReducer = (state=initialState, action: SignersActionType): S
   switch (action.type) {
     case SIGNERS_LOAD:
       return {...state, signers: action.signers};
+    case SIGNERS_SELECT:
+      return {...state, index: action.index};
     case SIGNERS_BALANCE:
       return {...state, signers: [
-        ...state.signers.slice(0, action.index),
+        ...state.signers.slice(0, state.index),
         {
-          ...state.signers[action.index],
+          ...state.signers[state.index],
           balance: action.balance
         },
-        ...state.signers.slice(action.index+1)
+        ...state.signers.slice(state.index+1)
       ]}
     default: return state;
   }
