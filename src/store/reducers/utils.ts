423605b839bcdfeddd66087fe3c87de4fe01d071
Žiga Franko <franko.ziga@gmail.com>
2021-06-07 16:47:23 +0200

    utils store now includes verification url

--- src/store/reducers/utils.ts
+++ src/store/reducers/utils.ts
@@ -1,21 +1,22 @@
 import { UtilsActionType } from "../actions/utils";
-import { UTILS_SET_NOTIFY, UTILS_SET_PROVIDER } from "../actionType";
+import { UTILS_SET_NOTIFY, UTILS_SET_PROVIDER, UTILS_SET_VERIFICATION_URL } from "../actionType";
 import { Provider } from '@reef-defi/evm-provider';
 
 export interface UtilsReducer {
   notify: (message: string) => void;
-  provider: Provider | undefined;
+  provider?: Provider;
+  verificationUrl?: string;
 }
 
 const initialUtilsState: UtilsReducer = {
   notify: (message: string) => {},
-  provider: undefined,
 }
 
 export const utilsReducer = (state=initialUtilsState, action: UtilsActionType): UtilsReducer => {
   switch (action.type) {
     case UTILS_SET_NOTIFY: return {...state, notify: action.function};
     case UTILS_SET_PROVIDER: return {...state, provider: action.provider};
+    case UTILS_SET_VERIFICATION_URL: return {...state, verificationUrl: action.url};
     default: return state;
   }
 } 
